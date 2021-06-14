a431f5db47225fc3a4a7cae02ad4771342478622
Mauricio Ale. Gonzalez <85379984+maucoderGit@users.noreply.github.com>
2021-06-13 23:01:41 -0400

    Fixing bugs in my JavaScript
    I had a lot of problems in my first attempts, all because of a problem in writing in JavaScript

--- Cajero Automatico/automatico.js
+++ Cajero Automatico/automatico.js
@@ -9,10 +9,57 @@ class Billete
 
 var caja = [];
 var entregado = [];
-caja.push(new Billete(50, 3));
-caja.push(new Billete(20, 2));
-caja.push(new Billete(10, 2));
+caja.push(new Billete(50, 10));
+caja.push(new Billete(20, 30));
+caja.push(new Billete(10, 20));
+caja.push(new Billete(100, 20));
+caja.push(new Billete(5, 20));
+var dinero = 0;
+var div = 0;
+var papeles = 0;
 
-var dinero = 210;
+var resultado = document.getElementById("resultado");
+var b = document.getElementById("extraer");
+b.addEventListener("click", entregarDinero);
 
-//11:21 Cajero automatico Clase 2
+function entregarDinero()
+{
+    var t = document.getElementById("cash");
+    dinero = parseInt(t.value);
+    for(var bi of caja)
+    {
+
+        if(dinero > 0)
+        {
+            div = Math.floor(dinero / bi.valor);
+            
+            if(div > bi.cantidad)
+            {
+                papeles = bi.cantidad;
+            }
+            else 
+            {
+                papeles = div;
+            }
+
+            entregado.push(new Billete(bi.valor, papeles));
+            dinero = dinero - (bi.valor *  papeles);
+            
+        }
+        
+
+    }
+    if(dinero > 0)
+    {
+        resultado.innerHTML = "Soy un cajero malo y no puedo darte esa cantidad :(";
+    }
+    else
+    {
+        for(var e of entregado)
+        {  
+            resultado.innerHTML = resultado.innerHTML + e.cantidad + " billetes de $" + e.valor + "<br />";
+        }
+    }
+}
+
+
